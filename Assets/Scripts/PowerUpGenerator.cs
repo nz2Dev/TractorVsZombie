993077af1f1d5b898ebdc0a797f9715011063da7
Nazar Pinonzhyk <nazar.pinonzhyk@gmail.com>
2022-06-20 14:29:22 +0300

    Add bounding box power up generator

---
+++ Assets/Scripts/PowerUpGenerator.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using Random = System.Random;
+
+public class PowerUpGenerator : MonoBehaviour {
+
+    public GameObject powerUpPrefab;
+    public LayerMask powerUpMask;
+    public int generationInterval = 4;
+
+    public Vector2 boundsStart;
+    public Vector2 boundsEnd;
+
+    private readonly Random _random = new();
+
+    private IEnumerator Start() {
+        while (true) {
+            yield return new WaitForSeconds(generationInterval);
+            if (isActiveAndEnabled) {
+                GenerateNextPowerUp();
+            }
+        }
+    }
+
+    private bool GenerateNextPosition(out Vector3 positionResult) {
+        var attempts = 0;
+        var generated = false;
+        
+        positionResult = Vector3.zero;
+        while (attempts < 5 ) {
+            attempts++;
+            
+            var horizontal = _random.Next((int)boundsStart.x, (int)boundsEnd.x);
+            var vertical = _random.Next((int)boundsStart.y, (int)boundsEnd.y);
+            var position = new Vector3(horizontal, 0, vertical);
+
+            if (!Physics.CheckSphere(position, 0.5f, powerUpMask)) {
+                positionResult = position;
+                generated = true;
+                break;
+            }
+        }
+
+        return generated;
+    }
+
+    public void GenerateNextPowerUp() {
+        if (GenerateNextPosition(out var position)) {
+            Instantiate(powerUpPrefab, position, Quaternion.identity);
+        }
+    }
+}
