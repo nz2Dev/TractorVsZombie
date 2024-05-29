94bd9f0d351b726650d4d015c8ea766e48ef2152
Nazar Pinonzhyk <nz2develop@gmail.com>
2024-05-29 12:14:47 +0300

    Add cargo physics wheels control

---
+++ Assets/Playground/CargoControl.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CargoControl : MonoBehaviour {
+    [SerializeField] private Transform carier;
+
+    private WheelControl[] wheels;
+
+    private void Start() {
+        wheels = GetComponentsInChildren<WheelControl>();
+    }
+
+    private void Update() {
+        var cargoToCarrier = carier.transform.position - transform.position;
+        var flatDirectionVector = Vector3.ProjectOnPlane(cargoToCarrier, Vector3.up);
+        var flatForwardVector = Vector3.ProjectOnPlane(transform.forward, Vector3.up);
+        var steerAngle = Vector3.SignedAngle(flatForwardVector, flatDirectionVector.normalized, Vector3.up);
+        foreach (var wheel in wheels) {
+            if (wheel.steerable) {
+                wheel.WheelCollider.steerAngle = steerAngle;
+            }
+        }
+    }
+}
