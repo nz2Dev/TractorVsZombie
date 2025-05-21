9c3179cb605df93dd89a146a777c856c39ca5208
Nazar Pinonzhyk <nz2develop@gmail.com>
2025-05-21 20:01:10 +0300

    Refactor trailer vehicle test

--- Assets/NewFolder/PlayTests/TrailerVehicleTest.cs
+++ Assets/NewFolder/PlayTests/TrailerVehicleTest.cs
@@ -9,10 +9,16 @@ using UnityEngine.TestTools.Utils;
 [TestFixture]
 public class TrailerVehicleTest {
 
+    private GameObject trailerVehicle;
+
+    [SetUp]
+    public void SetupTest() {
+        var trailerVehiclePrefab = Resources.Load<GameObject>("Trailer Vehicle");
+        trailerVehicle = Object.Instantiate(trailerVehiclePrefab);
+    }
+
     [UnityTest]
     public IEnumerator CreateTrailer() {
-        var trailerVehiclePrefab = Resources.Load("Trailer Vehicle");
-        var trailerVehicle = Object.Instantiate(trailerVehiclePrefab);
         yield return null;
         Assert.That(trailerVehicle, Is.Not.Null);
     }
@@ -20,15 +26,11 @@ public class TrailerVehicleTest {
     [UnityTest]
     public IEnumerator CreateTrailerNoJoint_StayAtPlace() {
         var initPosition = Vector3.zero;
-        var initRotation = Quaternion.identity;
+        trailerVehicle.transform.position = initPosition;
 
-        var trailerVehiclePrefab = Resources.Load<GameObject>("Trailer Vehicle");
-        var trailerVehicle = Object.Instantiate(trailerVehiclePrefab, initPosition, initRotation);
+        yield return WaitInitializationWithPauseGap();
+
         var hingeAnchor = trailerVehicle.GetComponent<HingeJoint>().anchor;
-
-        yield return new WaitForSecondsRealtime(0.5f);
-        yield return null;
-
         var targetPosition = initPosition - hingeAnchor;
         var vector3Comparer = new Vector3EqualityComparer(0.0001f);
         Assert.That(trailerVehicle.transform.position,
@@ -37,11 +39,19 @@ public class TrailerVehicleTest {
 
     [UnityTest]
     public IEnumerator CreateTrailerApartTarget_SnapsToTarget_NoVelocity() {
+
+        yield return WaitInitializationWithPauseGap();
         
+    }
 
-        yield return new WaitForSeconds(0.5f);
+    private IEnumerator WaitInitializationWithPauseGap() {
+        yield return new WaitForSecondsRealtime(0.5f);
         yield return null;
+    }
 
+    [TearDown]
+    public void TearDownTest() {
+        Object.Destroy(trailerVehicle);
     }
 
 }
