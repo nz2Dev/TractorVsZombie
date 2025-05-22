7889fb5d803aefdf1c9c67081b2500afd4a858c2
Nazar Pinonzhyk <nz2develop@gmail.com>
2025-05-22 11:01:00 +0300

    Add movement test to drive vehicle with test env

--- Assets/NewFolder/DriveVehicle.cs
+++ Assets/NewFolder/DriveVehicle.cs
@@ -1,6 +1,45 @@
 
+using System;
+
 using UnityEngine;
 
-public class DriveVehicle {
-    public Vector3 Position => Vector3.zero;
+public class DriveVehicle : IDisposable {
+
+    private readonly Transform transform;
+    private readonly Rigidbody rigidbody;
+    private readonly WheelCollider[] wheels;
+    
+    private WheelCollider BackWheelL => wheels[0];
+    private WheelCollider BackWheelR => wheels[1];
+    private WheelCollider FrontWheelL => wheels[2];
+    private WheelCollider FrontWheelR => wheels[3];
+
+    public DriveVehicle() {
+        var driveVehiclePrefab = Resources.Load<GameObject>("Drive Vehicle");
+
+        var vehicleGO = UnityEngine.Object.Instantiate(driveVehiclePrefab);
+        transform = vehicleGO.transform;
+        rigidbody = vehicleGO.GetComponent<Rigidbody>();
+        wheels = vehicleGO.GetComponentsInChildren<WheelCollider>();
+    }
+
+    public Vector3 Position => transform.position;
+
+    public void Brakes(float breakTorque) {
+        foreach (var wheel in wheels)
+            wheel.brakeTorque = breakTorque;
+    }
+
+    public void Gas(float motorTorque) {
+        foreach (var wheel in wheels)
+            wheel.motorTorque = motorTorque;
+    }
+
+    public void AddForce(Vector3 force) {
+        rigidbody.AddForce(force, ForceMode.Force);
+    }
+
+    public void Dispose() {
+        UnityEngine.Object.Destroy(transform.gameObject);
+    }
 }
--- Assets/NewFolder/PlayTests/DriveVehicleTest.cs
+++ Assets/NewFolder/PlayTests/DriveVehicleTest.cs
@@ -1,21 +1,42 @@
 using System.Collections;
+using System.IO;
+using System.Linq;
 
 using NUnit.Framework;
 
+using UnityEditor;
+
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.TestTools;
 using UnityEngine.TestTools.Utils;
 
 [TestFixture]
-public class DriveVehicleTest {
+public class DriveVehicleTest : IPrebuildSetup, IPostBuildCleanup {
+
+    private string originalScene;
+    private readonly string TestEnvironmentScenePath = Path.Combine(
+        "Assets", "NewFolder", "TestEnvironment.unity");
 
     private DriveVehicle driveVehicle;
-    private readonly Vector3EqualityComparer smallVectorComparer = new (0.0001f);
+    private readonly Vector3EqualityComparer smallVectorComparer = new(0.0001f);
+
+    public void Setup() {
+#if UNITY_EDITOR
+        if (EditorBuildSettings.scenes.Any(scene => scene.path == TestEnvironmentScenePath))
+            return;
+        var includedScenes = EditorBuildSettings.scenes.ToList();
+        includedScenes.Add(new EditorBuildSettingsScene(TestEnvironmentScenePath, true));
+        EditorBuildSettings.scenes = includedScenes.ToArray();
+#endif
+    }
 
     [UnitySetUp]
     public IEnumerator SetupUnityTest() {
-        driveVehicle = new DriveVehicle();
+        originalScene = SceneManager.GetActiveScene().path;
+        SceneManager.LoadScene(TestEnvironmentScenePath);
         yield return null;
+        driveVehicle = new DriveVehicle();
     }
 
     [UnityTest]
@@ -29,6 +50,46 @@ public class DriveVehicleTest {
         yield return new WaitForSecondsRealtime(0.5f);
         Assert.That(driveVehicle.Position,
             Is.EqualTo(Vector3.zero).Using(smallVectorComparer));
+    }
+
+    [UnityTest]
+    public IEnumerator ApplySmallForceOnBreaks_KeepsItsPosition() {
+        yield return new WaitForSecondsRealtime(0.5f);
+
+        driveVehicle.Brakes(1);
+        driveVehicle.AddForce(Vector3.back);
+        yield return new WaitForFixedUpdate();
+        yield return new WaitForFixedUpdate();
+
+        Assert.That(driveVehicle.Position, Is.EqualTo(Vector3.zero));
+    }
+
+    [UnityTest]
+    public IEnumerator ApplySubstantialForceNoBrakesWithLittleGas_MovesVehicle() {
+        yield return new WaitForSecondsRealtime(0.5f);
+
+        driveVehicle.Gas(1);
+        driveVehicle.Brakes(0);
+        for (int i = 0; i < 10; i++) {
+            driveVehicle.AddForce(Vector3.forward * 5000);
+            yield return new WaitForFixedUpdate();
+        }
+
+        Assert.That(driveVehicle.Position.z, Is.GreaterThan(0.1f));
+    }
+
+    [TearDown]
+    public void TearDownTest() {
+        driveVehicle.Dispose();
+        SceneManager.LoadScene(originalScene);
+    }
+    
+    public void Cleanup() {
+#if UNITY_EDITOR
+        EditorBuildSettings.scenes = EditorBuildSettings.scenes
+            .Where(scene => scene.path != TestEnvironmentScenePath)
+            .ToArray();
+#endif
     }
 
 }
