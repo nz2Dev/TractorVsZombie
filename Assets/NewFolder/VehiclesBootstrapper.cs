4a9c57a1a12d2b3f43b401e5c84c219a1171d9c0
Nazar Pinonzhyk <nz2develop@gmail.com>
2025-06-03 12:27:04 +0300

    Add simple input for implemente methods

--- Assets/NewFolder/VehiclesBootstrapper.cs
+++ Assets/NewFolder/VehiclesBootstrapper.cs
@@ -6,10 +6,9 @@ public class VehiclesBootstrapper : MonoBehaviour {
     
     [SerializeField] private VehicleEntity driveVehicle;
     [SerializeField] private VehicleEntity trailerVehicle;
-    [SerializeField] private float gasThrottle = 0;
 
     private List<VehicleEntity> vehicles;
-    private VehicleService vehicleSimulationService;
+    private VehicleService vehicleService;
     private VehicleView vehicleView;
 
     private void Start() {
@@ -18,19 +17,19 @@ public class VehiclesBootstrapper : MonoBehaviour {
 
         vehicles = new();
         vehicleView = new (container: transform);
-        vehicleSimulationService = new (physicsContainer: transform);
+        vehicleService = new (physicsContainer: transform);
 
         var driveVehiclePosition = Vector3.zero;
-        vehicleSimulationService.CreateVehicle(driveVehicle.baseSize, driveVehicle.wheelAxisDatas);
+        vehicleService.CreateVehicle(driveVehicle.baseSize, driveVehicle.wheelAxisDatas);
         vehicleView.AddVehicle(driveVehiclePosition, driveVehicle.baseGeometry, driveVehicle.wheelGeometry, driveVehicle.wheelAxisDatas);
         vehicles.Add(driveVehicle);
 
         var trailerVehiclePosition = new Vector3(0, 0, -2f);
-        vehicleSimulationService.CreateVehicle(trailerVehicle.baseSize, trailerVehicle.wheelAxisDatas, trailerVehiclePosition);
+        vehicleService.CreateVehicle(trailerVehicle.baseSize, trailerVehicle.wheelAxisDatas, trailerVehiclePosition);
         vehicleView.AddVehicle(trailerVehiclePosition, trailerVehicle.baseGeometry, trailerVehicle.wheelGeometry, trailerVehicle.wheelAxisDatas);
         vehicles.Add(trailerVehicle);
 
-        vehicleSimulationService.ConnectVehicleWithHinge(headVehicleIndex: 0, -0.7f, tailVehicleIndex: 1, 0.7f);
+        vehicleService.ConnectVehicleWithHinge(headVehicleIndex: 0, -0.7f, tailVehicleIndex: 1, 0.7f);
     }
     
     [ContextMenu("Preview")]
@@ -38,23 +37,28 @@ public class VehiclesBootstrapper : MonoBehaviour {
         while (transform.childCount > 0)
             DestroyImmediate(transform.GetChild(0).gameObject);
 
-        vehicleSimulationService = new (physicsContainer: transform);
-        vehicleSimulationService.CreateVehicle(driveVehicle.baseSize, driveVehicle.wheelAxisDatas);
+        vehicleService = new (physicsContainer: transform);
+        vehicleService.CreateVehicle(driveVehicle.baseSize, driveVehicle.wheelAxisDatas);
         
         vehicleView = new (container: transform);
         vehicleView.AddVehicle(Vector3.zero, driveVehicle.baseGeometry, driveVehicle.wheelGeometry, driveVehicle.wheelAxisDatas);
     }
 
     private void Update() {
-        vehicleSimulationService.SetVehicleGasThrottle(vehicleIndex: 0, gasThrottle);
+        const float maxSteerAngle = 45;
+        var gasInput = Input.GetAxis("Vertical");
+        var steerInput = Input.GetAxis("Horizontal");
+
+        vehicleService.SetVehicleGasThrottle(vehicleIndex: 0, gasInput);
+        vehicleService.SetVehicleSteer(vehicleIndex: 0, steerInput * maxSteerAngle);
 
         for (int vehicleIndex = 0; vehicleIndex < vehicles.Count; vehicleIndex++) {
             var vehicleData = vehicles[vehicleIndex];
-            var vehiclePose = vehicleSimulationService.GetVehiclePose(vehicleIndex);
+            var vehiclePose = vehicleService.GetVehiclePose(vehicleIndex);
             vehicleView.UpdateVehiclePose(vehicleIndex, vehiclePose);
 
             for (int wheelAxisIndex = 0; wheelAxisIndex < vehicleData.wheelAxisDatas.Length; wheelAxisIndex++) {
-                var wheelAxisPose = vehicleSimulationService.GetVehicleWheelAxisPose(vehicleIndex, wheelAxisIndex);
+                var wheelAxisPose = vehicleService.GetVehicleWheelAxisPose(vehicleIndex, wheelAxisIndex);
                 vehicleView.UpdateWheelAxisPose(vehicleIndex, wheelAxisIndex, wheelAxisPose);
             }    
         }
