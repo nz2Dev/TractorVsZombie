1227dbdc88cdbd5f9e94e230d2c0e57fab2028e9
Nazar Pinonzhyk <nz2develop@gmail.com>
2025-07-13 21:03:40 +0300

    Implement blockers generator giszmos debug

---
+++ Assets/FlowFields/BockersGenerator.cs
@@ -0,0 +1,51 @@
+using UnityEditor;
+
+using UnityEngine;
+
+public class BockersGenerator : MonoBehaviour {
+
+    [SerializeField] private FlowFieldsGrid grid;
+    
+    private void OnValidate() {
+        
+    }
+
+    void OnDrawGizmos() {
+        var collider = GetComponent<BoxCollider>();
+        var center = collider.bounds.center;
+        var extens = collider.bounds.extents;
+
+        Vector3 bottomLeft = center + new Vector3(-extens.x, 0, -extens.z);
+        Vector3 topLeft = center + new Vector3(-extens.x, 0, +extens.z);
+        Vector3 topRight = center + new Vector3(+extens.x, 0, +extens.z);
+        Vector3 bottomRight = center + new Vector3(+extens.x, 0, -extens.z);
+        Handles.DrawPolyLine(
+            bottomLeft,
+            topLeft,
+            topRight,
+            bottomRight,
+            bottomLeft
+        );
+
+        if (grid == null)
+            return;
+
+        var gridStart = grid.ConvertToGrid(bottomLeft);
+        var gridEnd = grid.ConvertToGrid(topRight);
+        var rowsSpan = Mathf.Max(gridEnd.x - gridStart.x, 1);
+        var columnSpan = Mathf.Max(gridEnd.y - gridStart.y, 1);
+        for (int row = 0; row < rowsSpan; row++) {
+            for (int column = 0; column < columnSpan; column++) {
+                var gridLocation = gridStart + new Vector2Int(row, column);
+                var gridWorld = grid.ConvertToWorld(gridLocation, atCenter: true);
+
+                var gridRay = new Ray(gridWorld + Vector3.up * 10, Vector3.down);
+                var raycasted = collider.Raycast(gridRay, out var _, maxDistance: float.MaxValue);
+                
+                Gizmos.color = raycasted ? Color.red : Color.white;
+                Gizmos.DrawWireSphere(gridWorld, 0.5f);
+            }
+        }
+    }
+
+}
