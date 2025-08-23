b822d17717a814cd68ae171af5ac7713d8bee41d
Nazar Pinonzhyk <nz2develop@gmail.com>
2025-08-24 00:01:31 +0300

    Fix look rotation velocity zero magnitude

--- Assets/NewFolder/Domain/LocalAvoidanceService.cs
+++ Assets/NewFolder/Domain/LocalAvoidanceService.cs
@@ -52,7 +52,11 @@ public class LocalAvoidanceService {
     public void GetAgentPositionAndRotation(int agentId, out Vector3 pos, out Quaternion rot) {
         var agent = agentRegistry[agentId];
         pos = agent.pos;
-        rot = Quaternion.LookRotation(agent.velocity, Vector3.up);
+        if (math.lengthsq(agent.velocity) > float.Epsilon) {
+            rot = Quaternion.LookRotation(agent.velocity, Vector3.up);
+        } else {
+            rot = Quaternion.identity;
+        }
     }
 
     public Quaternion GetAgentRotation(int agentId) {
