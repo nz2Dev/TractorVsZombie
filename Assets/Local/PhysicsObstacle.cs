eb5cd9c5343d67625ca94e4fa395e0a7838d6af8
Nazar Pinonzhyk <nz2develop@gmail.com>
2026-02-04 01:05:01 +0200

    Add PhysicsObstacle note commnets

--- Assets/Local/PhysicsObstacle.cs
+++ Assets/Local/PhysicsObstacle.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 
+// Is used as a hack to get a shape editor.
+// But PhysicsService and VehicleService use them as natural UnityPhysics collider as it is. Be careful!
 [RequireComponent(typeof(BoxCollider))]
 public class PhysicsObstacle : MonoBehaviour {
     public Vector3 bakedSize;
