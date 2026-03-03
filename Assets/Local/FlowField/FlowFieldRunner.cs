27a92f9945db6e88e7555290b957ef1fe9f55b80
Nazar Pinonzhyk <nz2develop@gmail.com>
2026-03-03 21:56:27 +0200

    Ensure only one flow fields runner

--- Assets/Local/FlowField/FlowFieldRunner.cs
+++ Assets/Local/FlowField/FlowFieldRunner.cs
@@ -1,17 +1,28 @@
 using UnityEngine;
 
 public class FlowFieldRunner : MonoBehaviour {
+
+    private static FlowFieldRunner _instance;
     
     private void Awake() {
-        FlowFieldSystem.Instance = new FlowFieldSystem();
+        if (_instance != null && _instance != this) {
+            Destroy(gameObject);
+            return;
+        }
+
+        _instance = this;
         DontDestroyOnLoad(gameObject);
+        FlowFieldSystem.Instance = new FlowFieldSystem();
     }
 
     private void Update() {
         FlowFieldSystem.Instance.Update();    
     }
 
-    private void OnDestroy() {
+    void OnDestroy() {
+        if (_instance == this)
+            _instance = null;
+
         FlowFieldSystem.Instance = null;
     }
 
