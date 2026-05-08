65521385c0683f4210a09e7a32470daa92875c93
Nazar Pinonzhyk <nz2develop@gmail.com>
2026-05-08 21:23:15 +0300

    Make find in scene an editor method

--- Assets/NewFolder/Scripts/Level/LevelPrototypeSource.cs
+++ Assets/NewFolder/Scripts/Level/LevelPrototypeSource.cs
@@ -4,16 +4,23 @@ using UnityEngine.Playables;
 public class LevelPrototypeSource : MonoBehaviour {
     
     [SerializeField] private PlayableDirector entranceCutscene;
+    [SerializeField] private PlayerPrototypeSource playerPrototypeSource;
+    [SerializeField] private EnemyPrototypeSource enemyPrototypeSource;
+    [SerializeField] private HeadquarterBuildingSource headquarterBuildingSource;
+
+    [ContextMenu("Find In Scene")]
+    public void FindInScene() {
+        playerPrototypeSource = GameObject.FindFirstObjectByType<PlayerPrototypeSource>(FindObjectsInactive.Include);
+        enemyPrototypeSource = GameObject.FindFirstObjectByType<EnemyPrototypeSource>(FindObjectsInactive.Include);
+        headquarterBuildingSource = GameObject.FindFirstObjectByType<HeadquarterBuildingSource>(FindObjectsInactive.Include);
+    }
     
     public LevelPrototype Get() {
-        var enemySource = GameObject.FindFirstObjectByType<EnemyPrototypeSource>(FindObjectsInactive.Include);
-        var playerSource = GameObject.FindFirstObjectByType<PlayerPrototypeSource>(FindObjectsInactive.Include);
-        var headquarterSource = GameObject.FindFirstObjectByType<HeadquarterBuildingSource>(FindObjectsInactive.Include);
         return new LevelPrototype {
             entranceCutscene = entranceCutscene,
-            enemyPrototype = enemySource.Get(),
-            playerPrototype = playerSource.Get(),
-            headquarterBuildingPrototype = headquarterSource.GetPrototype(),
+            enemyPrototype = enemyPrototypeSource.Get(),
+            playerPrototype = playerPrototypeSource.Get(),
+            headquarterBuildingPrototype = headquarterBuildingSource.GetPrototype(),
         };
     }
 }
