b68d8fa9c48f53303a0087e6bccd42c08dfa9e5b
Nazar Pinonzhyk <nz2develop@gmail.com>
2026-07-21 21:22:24 +0300

    Copy IsLosCorner method

--- Assets/Local/FlowField/FlowFieldLineOfSightPass.cs
+++ Assets/Local/FlowField/FlowFieldLineOfSightPass.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public static class FlowFieldLineOfSightPass {
-    
+
     public static void ComputeLineOfSight(FlowField flowField, Vector2Int goal, List<Vector2Int> wavefrontOutput) {
         wavefrontOutput.Clear();
         wavefrontOutput.Add(goal);
@@ -14,7 +14,42 @@ public static class FlowFieldLineOfSightPass {
         throw new NotImplementedException();
     }
 
-    internal static bool IsLosCorner(FlowField field, Vector2Int cell, Vector2Int test, Vector2Int goal) {
-        throw new NotImplementedException();
+    internal static bool IsLosCorner(FlowField field, Vector2Int cell, Vector2Int neighbor, Vector2Int goal) {
+        int dx = neighbor.x - cell.x;
+        if (dx != 0) {
+            int gy = goal.y - cell.y;
+            int gx = goal.x - cell.x;
+
+            if (gy == 0 || gx == 0)
+                return false;
+
+            if (gx < 0 && dx > 0 || gx > 0 && dx < 0)
+                return false;
+
+            var awayCell = new Vector2Int(cell.x, cell.y + Math.Sign(gy));
+            if (!field.IsInBounds(awayCell.x, awayCell.y))
+                return true;
+
+            var awayIsBlocked = field[awayCell.x, awayCell.y].cost > 1;
+            return !awayIsBlocked;
+        }
+        else {
+            int gx = goal.x - cell.x;
+            int gy = goal.y - cell.y;
+
+            if (gx == 0 || gy == 0)
+                return false;
+
+            int dy = neighbor.y - cell.y;
+            if (gy < 0 && dy > 0 || gy > 0 && dy < 0)
+                return false;
+
+            var awayCell = new Vector2Int(cell.x + Math.Sign(gx), cell.y);
+            if (!field.IsInBounds(awayCell.x, awayCell.y))
+                return true;
+
+            var awayIsBlocked = field[awayCell.x, awayCell.y].cost > 1;
+            return !awayIsBlocked;
+        }
     }
 }
