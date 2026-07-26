cc79dca4a778b7d41e68d07ad59357b1c1361c32
Nazar Pinonzhyk <nz2develop@gmail.com>
2026-07-26 22:09:11 +0300

    Fix flags not reset when field reintegrated

--- Assets/Local/FlowField/Core/FlowFieldIntegrator.cs
+++ Assets/Local/FlowField/Core/FlowFieldIntegrator.cs
@@ -9,7 +9,7 @@ public static partial class FlowFieldIntegrator {
     private static readonly List<Vector2Int> wavefrontBuffer = new();
 
     public static void Integrate(FlowField flowField, Vector2Int goal) {
-        ClearIntegratedCosts(flowField);
+        ClearField(flowField);
         wavefrontBuffer.Clear();
         if (losEnabled) {
             LineOfSightPass.ComputeLineOfSight(flowField, goal, wavefrontBuffer);
@@ -20,10 +20,12 @@ public static partial class FlowFieldIntegrator {
         VectorBuilderPass.ComputeFlow(flowField, goal);
     }
 
-    private static void ClearIntegratedCosts(FlowField field) {
+    private static void ClearField(FlowField field) {
         for (int x = 0; x < field.Size; x++) {
             for (int y = 0; y < field.Size; y++) {
                 field[x, y].integratedCost = 0;
+                field[x, y].flags = CellFlags.None;
+                field[x, y].flowVector = Vector2Int.zero;
             }
         }
     }
