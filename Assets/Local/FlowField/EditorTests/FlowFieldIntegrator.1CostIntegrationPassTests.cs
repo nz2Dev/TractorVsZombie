9b683663052d7ad5f83391265dc40b1552ed43c1
Nazar Pinonzhyk <nz2develop@gmail.com>
2026-07-27 18:27:12 +0300

    Add test to check integrated costs gradient

--- Assets/Local/FlowField/EditorTests/FlowFieldIntegrator.1CostIntegrationPassTests.cs
+++ Assets/Local/FlowField/EditorTests/FlowFieldIntegrator.1CostIntegrationPassTests.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 using NUnit.Framework;
 
 using UnityEngine;
@@ -28,5 +30,30 @@ public class FlowFieldIntegrator_1CostIntegrationPassTests {
         var cornerCost = flowField[0, 0].integratedCost;
         var edgeCost = flowField[0, radius].integratedCost;
         Assert.That(cornerCost, Is.GreaterThan(edgeCost));
+    }
+
+    //(y)
+    //
+    // 2  S S B 
+    // 1  S B .
+    // 0  G W . 
+    //
+    // #  0 1 2   (x)
+    [Test]
+    public void ComputeCost_FromLineOfSightWavefront_CostsIncreaseGradually() {
+        var goal = new Vector2Int(0, 0);
+        var wall = new Vector2Int(1, 0);
+        var field = new FlowField(3, new [] { wall });
+        var wavefront = new Vector2Int[] { new (1, 1), new (2, 2) };
+        field[0, 1] = new Cell { cost = 1, flags = CellFlags.HasLineOfSight, integratedCost = 1 };
+        field[0, 2] = new Cell { cost = 1, flags = CellFlags.HasLineOfSight, integratedCost = 2 };
+        field[1, 2] = new Cell { cost = 1, flags = CellFlags.HasLineOfSight, integratedCost = 3 };
+        field[1, 1] = new Cell { cost = 1, flags = CellFlags.WaveFrontBlocked, integratedCost = 2 };
+        field[2, 1] = new Cell { cost = 1, flags = CellFlags.WaveFrontBlocked, integratedCost = 4 };
+
+        FlowFieldIntegrator.CostIntegrationPass.ComputeCosts(field, goal, wavefront);
+
+        for (int x = 0; x < field.Size - 1; x++)
+            Assert.That(field[x + 1, 1].integratedCost - field[x, 1].integratedCost, Is.EqualTo(1));
     }
 }
