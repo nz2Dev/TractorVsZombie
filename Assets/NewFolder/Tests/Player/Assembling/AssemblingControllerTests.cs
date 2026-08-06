81075e7e51f99ca6c4964a49c3edc498966e522f
Nazar Pinonzhyk <nz2develop@gmail.com>
2026-08-06 12:51:19 +0300

    Add test for postponed spawn if near head position

--- Assets/NewFolder/Tests/Player/Assembling/AssemblingControllerTests.cs
+++ Assets/NewFolder/Tests/Player/Assembling/AssemblingControllerTests.cs
@@ -6,10 +6,13 @@ using Moq;
 
 using NUnit.Framework;
 
+using UnityEngine;
 using UnityEngine.Analytics;
 
 [TestFixture]
 public class AssemblingControllerTests {
+
+    private Mock<Action<int>> platformAddedCallbackMock;
     private Mock<AssemblingView> viewMock;
     private Mock<PlatformController> platformMock;
     private Mock<TruckController> truckMock;
@@ -22,21 +25,38 @@ public class AssemblingControllerTests {
         truckMock = MockHelper.CreateWithNulls<TruckController>();
         controller = new AssemblingController(
             viewMock.Object, platformMock.Object, truckMock.Object);
+        
+        platformAddedCallbackMock = new Mock<Action<int>>();
+        controller.OnPlatformAdded += platformAddedCallbackMock.Object;
     }
     
     [Test]
     public void Init_WithOneLoadout_FiresOneEvent() {
-        var OnPlatformAddedMock = new Mock<Action<int>>();
         var assemblingPrototype = new AssemblingPrototype { 
             initLoadoutPrototypes = new LoadoutPrototype[] { new () }
         };
 
-        controller.OnPlatformAdded += OnPlatformAddedMock.Object;
         controller.Init(assemblingPrototype);
 
-        OnPlatformAddedMock.Verify((m) => m(It.IsAny<int>()), Times.Once);
+        platformAddedCallbackMock.Verify((m) => m(It.IsAny<int>()), Times.Once);
     }
 
-   
+    [Test]
+    public void AddLoadout_AfterInitAtHeadPosition_DontSpawnImmediatelly() {
+        var headPosition = new Vector3(2, 0, 0);
+        truckMock.Setup(m => m.ReadVehiclePosition()).Returns(headPosition);
+        var addedLoadout = new LoadoutPrototype {
+            position = new Vector3(2, 0, 0),
+        };
+        var assemblingPrototype = new AssemblingPrototype { 
+            initLoadoutPrototypes = new LoadoutPrototype[] { new () }
+        };
+
+        controller.Init(assemblingPrototype);
+        controller.AddLoadout(headPosition, addedLoadout, true);
+        controller.Update();
+
+        platformAddedCallbackMock.Verify(m => m(It.IsAny<int>()), Times.Once);
+    }
 
 }
