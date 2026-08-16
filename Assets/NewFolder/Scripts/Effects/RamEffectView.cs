d05b16a56847cc93b4f6ab7c788b8802b086db21
Nazar Pinonzhyk <nz2develop@gmail.com>
2026-08-16 20:45:44 +0300

    Remove limits on ram effect impact

--- Assets/NewFolder/Scripts/Effects/RamEffectView.cs
+++ Assets/NewFolder/Scripts/Effects/RamEffectView.cs
@@ -6,33 +6,22 @@ using UnityEngine;
 
 public class RamEffectView {
 
-    internal class State {
-        public float lastImpactTime;
-    }
-    
     private Dictionary<int, AudioSource> audioSourceRegistry = new ();
-    private Dictionary<int, State> stateRegistry = new ();
 
     public RamEffectView(SoundManager soundManager) {
-        
     }
 
     public void AddEffect(int entityId, AudioSource audioSourcePrefab) {
         audioSourceRegistry[entityId] = GameObject.Instantiate(audioSourcePrefab);
-        stateRegistry[entityId] = new State();
     }
 
     public void ShowImpact(int entityId, Vector3 position, int times, AudioClip[] audioClips) {
-        var impactLimit = Random.Range(1, 1);
+        var impactLimit = 3;
         var maxImpacts = Mathf.Min(times, impactLimit);
         var audioSource = audioSourceRegistry[entityId];
         audioSource.transform.position = position;
-        var state = stateRegistry[entityId];
-        if (Time.time - state.lastImpactTime > 0f) {
-            state.lastImpactTime = Time.time;
-            for (int i = 0; i < maxImpacts; i++) {
-                audioSource.PlayOneShot(SelectRandom(audioClips));
-            }
+        for (int i = 0; i < maxImpacts; i++) {
+            audioSource.PlayOneShot(SelectRandom(audioClips));
         }
     }   
 
