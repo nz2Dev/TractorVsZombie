6d1960c6993f1e1f8bf9500be5c4686c7e8a0ecd
Nazar Pinonzhyk <nz2develop@gmail.com>
2026-08-23 11:43:52 +0300

    Add force mode to explosion config and adjust some data

--- Assets/NewFolder/Scripts/Engine/ExplosionConfig.cs
+++ Assets/NewFolder/Scripts/Engine/ExplosionConfig.cs
@@ -1,8 +1,11 @@
 using System;
 
+using UnityEngine;
+
 [Serializable]
 public struct ExplosionConfig {
     public float radius;
     public float force;
     public float upwardModifier;
+    public ForceMode forceMode;
 }
