9af0a4be0776a6b89c79b36c001a79241883b4ef
Nazar Pinonzhyk <nz2develop@gmail.com>
2026-08-23 13:30:32 +0300

    Add some more collision behavior tests

--- Assets/Playgrounds/PhysicsCollisions/PhysicsCollisonsTests.cs
+++ Assets/Playgrounds/PhysicsCollisions/PhysicsCollisonsTests.cs
@@ -9,7 +9,7 @@ using UnityEngine.TestTools;
 public class PhysicsCollisonsTests {
     
     [UnityTest]
-    public IEnumerator Run() {
+    public IEnumerator Run_IsKinematic_DoesntRegister() {
         CreateTargetCollider(new Vector3(2, 0, 0));
         var sensor = CreateSensorCollider(new Vector3(0, 0, 0), useGravity: false, isKinematic: true);
         yield return new WaitForSeconds(1);
@@ -19,21 +19,54 @@ public class PhysicsCollisonsTests {
 
         Assert.That(sensor.transform.position, Is.EqualTo(new Vector3(1.5f, 0, 0)));
         var eventsHandler = sensor.GetComponent<CollisionEventsHandler>();
+        Assert.That(eventsHandler.collisionEntered, Is.False);
+    }
+
+    [UnityTest]
+    public IEnumerator Run_RegularSpaceConflict_SettlesBoundaries() {
+        CreateTargetCollider(new Vector3(2, 0, 0), withRigidbody: true, useGravity: false);
+        var sensor = CreateSensorCollider(new Vector3(0, 0, 0), useGravity: false, isKinematic: false, mass: 10);
+        yield return new WaitForSeconds(1);
+
+        sensor.transform.position = new Vector3(1.5f, 0, 0);
+        yield return new WaitForSeconds(1);
+
+        Assert.That(sensor.transform.position, Is.Not.EqualTo(new Vector3(1.5f, 0, 0)));
+        var eventsHandler = sensor.GetComponent<CollisionEventsHandler>();
         Assert.That(eventsHandler.collisionEntered, Is.True);
     }
 
-    private static GameObject CreateSensorCollider(Vector3 position, bool useGravity, bool isKinematic) {
+    [UnityTest]
+    public IEnumerator Run_ByVelocity_GeneratesGreaterVelocityToAffected() {
+        CreateTargetCollider(new Vector3(2, 0, 0), withRigidbody: true, useGravity: false);
+        var sensor = CreateSensorCollider(new Vector3(0, -0.25f, 0), useGravity: false, isKinematic: false, mass: 50);
+        yield return new WaitForSeconds(1);
+
+        sensor.GetComponent<Rigidbody>().linearVelocity = new Vector3(5, 0, 0);
+        yield return new WaitForSeconds(1);
+
+        Assert.That(sensor.transform.position, Is.Not.EqualTo(new Vector3(1.5f, 0, 0)));
+        var eventsHandler = sensor.GetComponent<CollisionEventsHandler>();
+        Assert.That(eventsHandler.collisionEntered, Is.False);
+    }
+
+    private static GameObject CreateSensorCollider(Vector3 position, bool useGravity, bool isKinematic, float mass = 1) {
         var gameObject = new GameObject("sensor collider", typeof(Rigidbody), typeof(SphereCollider), typeof(CollisionEventsHandler));
         gameObject.transform.position = position;
         var rigidbody = gameObject.GetComponent<Rigidbody>();
         rigidbody.useGravity = useGravity;
         rigidbody.isKinematic = isKinematic;
+        rigidbody.mass = mass;
         return gameObject;
     }
 
-    private static GameObject CreateTargetCollider(Vector3 position) {
+    private static GameObject CreateTargetCollider(Vector3 position, bool withRigidbody = false, bool useGravity = false) {
         var gameObject = new GameObject("target collider", typeof(SphereCollider));
         gameObject.transform.position = position;
+        if (withRigidbody) {
+            var rigidbody = gameObject.AddComponent<Rigidbody>();
+            rigidbody.useGravity = useGravity;
+        }
         return gameObject;
     }
 
